6c8f3f4d50f087f0287fd79bce86eb907c3f8de0
wangs.wang <wangs.wang@qunar.com>
2015-12-09 18:16:01 +0800

    build project

---
+++ java/src/main/java/com.vishcn.test/Test.java
@@ -0,0 +1,12 @@
+package com.vishcn.test;
+
+/**
+ * @author Vishcn
+ * @since 2015年12月09日
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("I'm test code");
+    }
+}
